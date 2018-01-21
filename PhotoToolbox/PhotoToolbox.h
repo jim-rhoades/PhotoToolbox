ec9f057035a921393692eab5a4d7c5bd80e6e0f1
Jim Rhoades <jim@crushapps.com>
2018-01-20 20:15:12 -0500

    Initial Commit

---
+++ PhotoToolbox/PhotoToolbox.h
@@ -0,0 +1,19 @@
+//
+//  PhotoToolbox.h
+//  PhotoToolbox
+//
+//  Created by Jim Rhoades on 1/20/18.
+//  Copyright © 2018 Crush Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoToolbox.
+FOUNDATION_EXPORT double PhotoToolboxVersionNumber;
+
+//! Project version string for PhotoToolbox.
+FOUNDATION_EXPORT const unsigned char PhotoToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoToolbox/PublicHeader.h>
+
+
